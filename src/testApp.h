b34f87f9521d929a8f4b7ac01eb66f1cc979c1fe
Gerry Straathof <nosemonger@gmail.com>
2011-09-14 22:55:11 -0600

    first attempt
    a revisiting of a hack found here:
    https://github.com/chaosct/ofxKinect/commit/5c3571bd524e9d2f7400cdd65dee
    7126b9df30e9 but built for the most modern version of ofxkinect,
    designed for 007.

---
+++ src/testApp.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxKinect.h"
+
+class testApp : public ofBaseApp {
+	public:
+
+		void setup();
+		void update();
+		void draw();
+		void exit();
+	
+		void drawPointCloud();
+
+		void keyPressed  (int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		
+		/// start/stop recording and playback,
+		/// make sure you don't record and playback simultaneously 
+		/// using the same file!!!
+		void startRecording();
+		void stopRecording();
+		void startPlayback();
+		void stopPlayback();
+
+		ofxKinect 			kinect;
+		ofxKinect 			kinect1;
+	
+		ofxKinectRecorder 	kinectRecorder;
+		ofxKinectPlayer 	kinectPlayer;
+		
+		/// used to switch between the live kinect and the recording player
+		ofxBase3DVideo* 	kinectSource;
+
+	ofxCvColorImage		colorImg;
+	ofxCvColorImage		colorImg2;
+
+		ofxCvGrayscaleImage grayImage;
+		ofxCvGrayscaleImage grayImage2;// grayscale depth image
+		ofxCvGrayscaleImage grayThreshNear;		// the near thresholded image
+		ofxCvGrayscaleImage grayThreshFar;		// the far thresholded image
+
+	ofxCvContourFinder 	contourFinder;
+	ofxCvContourFinder 	contourFinder2;
+		
+		bool				bThreshWithOpenCV;
+		bool				bDrawPointCloud;
+
+		int 				nearThreshold;
+		int					farThreshold;
+
+		int					angle;
+		
+		int 				pointCloudRotationY;
+		
+		bool 				bRecord;
+		bool 				bPlayback;
+};
